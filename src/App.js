089e77f2756f69e111f09865f54f852bf44f1389
余传兴 <yuchuanxing@xiaohongshu.net>
2025-09-10 12:56:05 +0800

    Fix React Router basename configuration for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
         <main className="main-content">
